49213af19b5893a6be9208eee152cdf798ea69b4
ANMOL <43845658+Anmol270900@users.noreply.github.com>
2019-06-28 00:57:02 +0530

    Updates

---
+++ precache-manifest.25622c686980226fa10d7cf24f937a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a7d05bb11daa31c6cdb51b9da69c9a",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/repository-name/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ed6ae65a435d6d0e4093",
+    "url": "/repository-name/static/css/main.ac1ee749.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/repository-name/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "ed6ae65a435d6d0e4093",
+    "url": "/repository-name/static/js/main.01a1c95b.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "c120aff762b96bffbe807eb90e3f2aa8",
+    "url": "/repository-name/static/media/Wonderingserif-Regular.c120aff7.woff"
+  }
+]);
